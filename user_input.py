171d72fb70ff8b96a63876e0564a250bf7dd86d8
Agape77 <159944657+Agape77@users.noreply.github.com>
2024-02-25 11:48:48 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -5,3 +5,10 @@ location = input("Where do you live?")
 print("Hello " + name)
 print("you are "+str(age)+" years old" , end= ' ')
 print("and live in " + location)
+
+
+What is your name?Camille
+How old are you?21
+Where do you live?Umoja
+Hello Camille
+you are 21 years old and live in Umoja
